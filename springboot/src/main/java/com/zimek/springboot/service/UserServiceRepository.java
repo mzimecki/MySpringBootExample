0c167f34443a883c93f6e684f7319900e3eb5881
mzimecki <marcin.zimecki@gmail.com>
2014-07-04 12:37:44 +0200

    Unused interfaces removed

--- springboot/src/main/java/com/zimek/springboot/service/UserServiceRepository.java
+++ springboot/src/main/java/com/zimek/springboot/service/UserServiceRepository.java
@@ -3,9 +3,8 @@ package com.zimek.springboot.service;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
-public interface UserServiceRepository extends CrudRepository<User, Long>, JpaRepository<User, Long> {
+public interface UserServiceRepository extends JpaRepository<User, Long> {
 	
 	public List<User> findAll();
 	public User save(User user);
